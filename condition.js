5661a05c85beb70c06acbe2e2000cd830aeff53f
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2023-12-01 22:59:43 +0530

    Loops Basic

---
+++ condition.js
@@ -0,0 +1,30 @@
+//In JavaScript, you can use the if...else statement to conditionally execute code based on a specified condition.
+
+// //The "if" statement in JavaScript is used to execute a block of code if a certain condition is met. The "else" clause is used to execute a block of code if the condition is not met.
+
+// let num = 5;
+// if(num<4){
+//     console.log("Greater")
+// }else{
+//     console.log("Not Greater")
+// }
+
+// console.log('num:', num)
+
+// let age = 18
+// if(age <= 15){
+//     console.log("Adult")
+    
+// }else{
+//     console.log("Not Adult")
+// }
+// console.log('age:', age)
+
+
+//while loop
+let value = 10
+while(value<=15){
+    console.log(value)
+    value++
+}
+
